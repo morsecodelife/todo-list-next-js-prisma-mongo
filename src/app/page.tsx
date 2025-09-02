a31d7752938f11609c915a3a646ccbfcece6047a
morsecodelife <morsecodelife@gmail.com>
2025-09-02 15:39:45 +0700

    fix: update header styles for improved visual consistency and adjust margin in container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,8 +17,12 @@ export default async function Home() {
       <header className="flex md:flex-row justify-between items-center p-4 sticky top-0 bg-white/15 z-10 backdrop-blur-xl shadow-sm">
         {/* Logo + Title */}
         <div className="flex items-center gap-2">
-          <ListTodo className="h-6 w-6 md:h-7 md:w-7 text-emerald-500" />
-          <h1 className="font-bold text-2xl md:text-2xl tracking-tight">
+          <ListTodo className="h-6 w-6 md:h-7 md:w-7 text-emerald-600" />
+          <h1
+            className="font-bold text-2xl md:text-2xl tracking-tight
+             bg-gradient-to-r from-emerald-600 via-emerald-400 to-emerald-200/20
+             bg-clip-text text-transparent"
+          >
             TODO
           </h1>
         </div>
@@ -40,7 +44,7 @@ export default async function Home() {
         </Link>
       </header>
 
-      <div className="container pb-36 mx-auto grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 py-4 px-4 md:px-0 mt-15">
+      <div className="container pb-36 mx-auto grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 py-4 px-4 md:px-0 mt-6">
         {todos.length === 0 && <TodoBlank />}
         {todos.map((todo) => (
           <Todo
